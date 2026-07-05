91b35519d943e0929e2c5f55c530ed6e2f4022be
W <supakitu@yandex.ru>
2026-07-05 15:40:52 +0300

    interface OnDebounceListener

--- app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
+++ app/src/main/java/ru/yandex/practicum/contacts/utils/android/Debouncer.java
@@ -13,10 +13,10 @@ public class Debouncer {
     private static final int MESSAGE_ID = 1;
     private static final int DELAY = 500;
 
-    private final MainViewModel viewModel;
+    private final OnDebounceListener listener;
 
-    public Debouncer(MainViewModel viewModel) {
-        this.viewModel = viewModel;
+    public Debouncer(OnDebounceListener listener) {
+        this.listener = listener;
     }
 
     private final Handler handler = new Handler(Looper.getMainLooper()) {
@@ -30,15 +30,13 @@ public class Debouncer {
         }
     };
 
-    // метод отправляет сообщение, для обновления данных с задержкой в 500мс
     public void updateValue(String value) {
         final Message message = Message.obtain(handler, MESSAGE_ID, value);
         handler.removeMessages(MESSAGE_ID);
         handler.sendMessageDelayed(message, DELAY);
     }
 
-    // выполнить действие по прошествии 500мс, если нового события в течении 500мс не было отправлено
     private void doOnDebounce() {
-        viewModel.search();
+        listener.onDebounce();
     }
 }
